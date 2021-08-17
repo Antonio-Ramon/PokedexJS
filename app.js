f0fa219e7d1ceda5982009d48c1546f0644070ab
Antonio Ramon <antonio-ramon-dev@outlook.com>
2021-08-17 12:06:29 -0300

    Finalizado(?)

--- app.js
+++ app.js
@@ -11,16 +11,26 @@ const fetchPokemon = () => {
 
     Promise.all(pokemonPromises)
         .then(pokemons => {
-            // console.log(pokemons)
 
-            //reduce uitliza-se quando é necessário transformar um Array em outro tipo de dado
+            //REDUCE e MAP uitlizam-se quando é necessário transformar um Array em outro tipo de dado
             const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
+                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
                 accumulator += 
-                `<li class="card">${pokemon.name}</li>`
+                `<li class="card ${types[0]}">
+                <img class="card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png"/>
+                    <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                    <p class= "card-subtitle">${types.join(' | ')}</p>
+                </li>`
+
+
                 return accumulator
             }, '')
 
-            console.log(lisPokemons)
+            //linkando elementos do HTML no JS
+            const ul = document.querySelector ('[data-js="pokedex"]') 
+
+            ul.innerHTML = lisPokemons
         })
 }
 
