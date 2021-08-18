11b0f85e62fcaf1178812df9f5708599997069da
Antonio Ramon <antonio-ramon-dev@outlook.com>
2021-08-17 21:23:00 -0300

    Refatoramento Final

--- app.js
+++ app.js
@@ -1,37 +1,32 @@
-const fetchPokemon = () => {
-    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
-    const pokemonPromises = []
+// metodo Fetch usado para requisições AJAX Assincrono (buscar dados direto da rede sem a necessidade de recarregar a página)
+const generatePokemonPromises = () => Array(150).fill().map((_, index) => 
+    fetch(getPokemonUrl(index + 1)).then(response => response.json()))
 
-    //pegar todos os pokemons e jogar no array pokemonPromises
-    for (let i = 1; i <= 150; i++) {
-        // metodo Fetch usado para requisições AJAX Assincrono (buscar dados direto da rede sem a necessidade de recarregar a página)
-        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))  
-    }
+//REDUCE e MAP uitlizam-se quando é necessário transformar um Array em outro tipo de dado
+const generateHTML = pokemons => pokemons.reduce((accumulator, { name, id, types }) => {
+        const elementTypes = types.map(typeInfo => typeInfo.type.name)
 
-    Promise.all(pokemonPromises)
-        .then(pokemons => {
+        accumulator += `
 
-            //REDUCE e MAP uitlizam-se quando é necessário transformar um Array em outro tipo de dado
-            const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
-                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+        <li class="card ${elementTypes[0]}">
+        <img class="card-image" alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png"/>
+            <h2 class="card-title">${id}. ${name}</h2>
+            <p class= "card-subtitle">${elementTypes.join(' | ')}</p>
+        </li>`
 
-                accumulator += 
-                `<li class="card ${types[0]}">
-                <img class="card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png"/>
-                    <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-                    <p class= "card-subtitle">${types.join(' | ')}</p>
-                </li>`
+        return accumulator
+    }, '')
 
-
-                return accumulator
-            }, '')
-
-            //linkando elementos do HTML no JS
-            const ul = document.querySelector ('[data-js="pokedex"]') 
-
-            ul.innerHTML = lisPokemons
-        })
+//linkando elementos do HTML no JS
+const insertPokemonIntoPage = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]')
+    ul.innerHTML = pokemons
 }
 
-fetchPokemon()
+const pokemonPromises = generatePokemonPromises()
+
+Promise.all(pokemonPromises)
+    .then(generateHTML)
+    .then(insertPokemonIntoPage)
